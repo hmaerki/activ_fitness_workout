58e39786ce8489283e59ba8707702d1829dcfe06
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-03-14 09:02:12 +0100

    Extract svg

--- main.py
+++ main.py
@@ -142,8 +142,8 @@ class FitnessApp:
             card.classList.remove("done")
 
         btn_done = document.getElementById("btn-done")
-        SVG_LEFT = '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"/><polyline points="12 19 5 12 12 5"/></svg>'
-        SVG_RIGHT = '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"/><polyline points="12 5 19 12 12 19"/></svg>'
+        SVG_LEFT = '<svg width="22" height="22"><use href="#arrow-left"/></svg>'
+        SVG_RIGHT = '<svg width="22" height="22"><use href="#arrow-right"/></svg>'
         if exercise.get("done"):
             btn_done.innerHTML = f'{SVG_LEFT} Undo Done'
         else:
